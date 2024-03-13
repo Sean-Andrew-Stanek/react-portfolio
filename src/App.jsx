003027c6c4d97f07ef13d7fbf079e6515cf08a98
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-03-13 22:30:36 +0900

    Profile Image css and color changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import React from 'react';
+import {React, useState} from 'react';
 
 function App() {
     
@@ -15,14 +15,24 @@ function App() {
     );
 }
 
+let colors = ['red', 'yellow', 'green', 'blue'];
+
 function Home() {
+    const [colorIndex, setColorIndex] = useState(0);
+
+    let iterateColor = () => {
+        setColorIndex(colorIndex+1);
+    };
+
     return (
         <div>
             <div className='image-holder'>
-                <img src='./Perlin-Background-300-300.png' className='profile-image' />
+                <img src='./Perlin-Background-300-300.png' className='profile-image tintable'/>
+                <div className='profile-image' style={{borderRadius: '50%', width: '300px', height: '300px', backgroundColor: colors[colorIndex%4], opacity: .4}} />
                 <img src='./Portrait-300-300.png' className='profile-image' />
                 <img src='./Gold-Ring-300-300.png' className='profile-image' />
             </div>
+            <button onClick={iterateColor}>Change Color</button>
         </div>
     );
 }
