19e154582cdd008033b1617549effa9239b57a87
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-03-24 00:39:34 +0900

    main-view avatar and text bubble

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,8 +5,12 @@ export const MainView = () => {
 
     return (
         <div className='main-view-container'>
-            <div className='placeholder'>
-                This is the main view
+            <div className='mv-avatar-container'>
+                <img src = 'Question-200-350.png' style={{width:'35%', marginBottom: '5px'}}/>
+                <img src = 'Avatar-150-450.png'  style={{width:'100%'}}/>
+            </div>
+            <div className='mv-text-bubble'>
+                
             </div>
         </div>
     );
