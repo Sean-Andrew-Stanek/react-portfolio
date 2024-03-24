9e3382326452be2eb03896fdcda4dc3d82515cce
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-03-24 22:24:35 +0900

    mainView scss tweaks and global strings location

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,26 @@
 import {React} from 'react';
 import './main-view.scss';
+import '../../styles/avatar.scss';
+import { mainViewStrings as strings} from '../../utils/strings';
 
 export const MainView = () => {
 
     return (
         <div className='main-view-container'>
-            <div className='mv-avatar-container'>
-                <img src = 'Question-200-350.png' style={{width:'35%', marginBottom: '5px'}}/>
-                <img src = 'Avatar-150-450.png'  style={{width:'100%'}}/>
+            <div className='avatar-container'>
+                <img src = 'Question-200-350.png' className='avatar-above-image'/>
+                <img src = 'Avatar-150-450.png'  className='avatar-image'/>
             </div>
             <div className='mv-text-bubble'>
-                
+                <div style={{margin: '0 auto'}}>
+                    {strings.greeting}
+                </div>
+                <div style={{margin: '0 10px'}}>
+                    {strings.body}
+                </div>
+                <div style={{margin: '0 10px'}}>
+                    {strings.closing}
+                </div>
             </div>
         </div>
     );
---
+++ src/utils/strings.js
@@ -0,0 +1,10 @@
+//Global Strings for easy editing
+
+// Main View
+export const mainViewStrings = {
+    greeting: 'Stay a while and listen!',
+    body: 'I am a web designer and programmer coming from veterancy in the teaching field.  With tools used in MERN / MARN stacks ' +
+    'as well as skills ranging from C# to GIMP and AWS, I am prepared for any technical challenge.  Years of working with young children ' +
+    'has given me soft-skills such as problem solving, empathy and team-communication.',
+    closing: 'Click the buttons above to start to see how I can help you fulfill your projects.  You can also click the buttons below to contact me.'
+};
