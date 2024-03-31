db61c28dccadf77ac48a60464b4cc3260ed16f63
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-04-01 00:01:45 +0900

    Reduced code considerably

--- src/components/skills-view/skills-view.jsx
+++ src/components/skills-view/skills-view.jsx
@@ -9,45 +9,31 @@ export const SkillsView = () => {
     const [dataVisible, setDataVisible] = useState(true);
     const [devVisible, setDevVisible] = useState(true);
 
-    const mapSkills = (skillList, visibilityToggle) => {
+    const skillTree = (skillList, visibilityToggle, setVisibilityToggle, skillListHeaderText) => {
         return (
-            skillList.map((skill, index) => {
-                {return visibilityToggle && <div key={skillList+index+''}>{skill}</div>;}
-            })
+            <>
+                <div className = 'skill-tree-header'>
+                    {skillListHeaderText}
+                    <div className = 'skill-tree-expander' onClick={() => setVisibilityToggle(!visibilityToggle)} >
+                        <div className='skill-plus-sign' />
+                    </div>
+                </div>
+                {
+                    skillList.map((skill, index) => {
+                        {return visibilityToggle && <div key={`${skill}+${index}`}>{skill}</div>;}
+                    })
+                }
+            </>
         );
     };
 
     return (
         <div className='main-container'>
             <div className='skill-container'>
-                <div className = 'skill-tree-header'>
-                    Front End Skills
-                    <div className = 'skill-tree-expander' onClick={() => setFrontEndVisible(!frontEndVisible)} >
-                        <div className='skill-plus-sign' />
-                    </div>
-                </div>
-                {mapSkills(skills.frontEndSkills, frontEndVisible)}
-                <div className = 'skill-tree-header'>
-                    Back End Skills
-                    <div className = 'skill-tree-expander' onClick={() => setBackEndVisible(!backEndVisible)}>
-                        <div className='skill-plus-sign' />
-                    </div>
-                </div>
-                {mapSkills(skills.backEndSkills, backEndVisible)}
-                <div className = 'skill-tree-header'>
-                    Data Skills
-                    <div className = 'skill-tree-expander' onClick={() => setDataVisible(!dataVisible)}>
-                        <div className='skill-plus-sign' />
-                    </div>
-                </div>
-                {mapSkills(skills.dataSkills, dataVisible)}
-                <div className = 'skill-tree-header'>
-                    Development Skills
-                    <div className = 'skill-tree-expander' onClick={() => setDevVisible(!devVisible)}>
-                        <div className='skill-plus-sign' />
-                    </div>
-                </div>
-                {mapSkills(skills.devSkills, devVisible)}
+                {skillTree(skills.frontEndSkills, frontEndVisible, setFrontEndVisible, 'Front End Skills')}
+                {skillTree(skills.backEndSkills, backEndVisible, setBackEndVisible, 'Back End Skills')}
+                {skillTree(skills.dataSkills, dataVisible, setDataVisible, 'Data Skills')}
+                {skillTree(skills.devSkills, devVisible, setDevVisible, 'Development Skills')}
             </div>
         </div>
     );
