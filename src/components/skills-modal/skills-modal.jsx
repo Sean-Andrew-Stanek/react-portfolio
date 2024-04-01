8b43a83e61ee790e51842f8cfe3109c660fcb12c
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-04-01 11:49:46 +0900

    created skill-modal.jsx

---
+++ src/components/skills-modal/skills-modal.jsx
@@ -0,0 +1,16 @@
+//This modal expands a skill clicked on in the skills-view component
+//It displays all projects which contain the skill
+
+import {React} from 'react';
+
+export const SkillsModal = () => {
+
+    return (
+
+        <div className='skill-modal-container'>
+            
+        </div>
+
+    );
+
+};
