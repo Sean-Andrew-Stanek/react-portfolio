85d51896832717c41aa00bb54e2639cf95736724
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-04-16 11:10:37 +0900

    skills modal cleaner and nav arrows

--- src/components/skills-modal/skills-modal.jsx
+++ src/components/skills-modal/skills-modal.jsx
@@ -19,7 +19,9 @@ export const SkillsModal = ({modalData, setModalData}) => {
 
     
     
-
+    /*
+    *   All projects with the requested skill from modalData.data
+    */
     const filteredList = projects.filter(project =>
         project.skills.frontend.includes(modalData.data) ||
         project.skills.backend.includes(modalData.data) ||
@@ -29,10 +31,16 @@ export const SkillsModal = ({modalData, setModalData}) => {
     return (
 
         <div className='skill-modal-container'>
-            {filteredList.map((project, index) =>{
-                return <SkillProjectSummary setModalData={setModalData} project={project} key={`${project.name}${index}`}/>;
-            })}
-
+            <div className='text-box-container' style={{width: '100%', height: '100%'}}>
+                <div className= 'text-box-border' />
+                <div className='text-box-content skill-modal-projects'>
+                    {filteredList.map((project, index) =>{
+                        return <SkillProjectSummary setModalData={setModalData} project={project} key={`${project.name}${index}`}/>;
+                    })}
+                </div>
+                <img className='text-box-nav-up' src='/react-portfolio/Nav-Arrow-1024-1024.png'/>
+                <img className='text-box-nav-down' src='/react-portfolio/Nav-Arrow-1024-1024.png'/>
+            </div>
         </div>
 
     );
