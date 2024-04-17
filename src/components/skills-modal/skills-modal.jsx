cc7711d56c18a0f3a1c352e42ef3048919cbccda
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-04-17 11:53:19 +0900

    nav helper functionality added

--- src/components/skills-modal/skills-modal.jsx
+++ src/components/skills-modal/skills-modal.jsx
@@ -9,18 +9,17 @@
 //-skills: array of skills
 //-summary: short project summary
 
-import {React} from 'react';
+import {React, useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import { SkillProjectSummary } from './skill-project-summary/skill-project-summary';
 import './skills-modal.scss';
 import { projects } from '../../utils/porfolio-projects';
+import '../../styles/styles.scss';
 
 export const SkillsModal = ({modalData, setModalData}) => {
-
-    
     
     /*
-    *   All projects with the requested skill from modalData.data
+    *   Filters projects with requested skill
     */
     const filteredList = projects.filter(project =>
         project.skills.frontend.includes(modalData.data) ||
@@ -28,18 +27,45 @@ export const SkillsModal = ({modalData, setModalData}) => {
         project.skills.other.includes(modalData.data)
     );
 
+    /*
+    *   Nav Arrow Functionality
+    */
+
+    const scrollListener = useRef(null);
+    const [isTop, setIsTop] = useState(true);
+    const [isBottom, setIsBottom] = useState(true);
+
+    /*
+    *   Controls nav helper opacity
+    */
+    const handleScroll = () => {
+        const capstoneScroller = scrollListener.current;
+
+        setIsTop(capstoneScroller.scrollTop === 0);
+            
+        setIsBottom(capstoneScroller.scrollTop >= (capstoneScroller.scrollHeight - capstoneScroller.clientHeight));
+
+    };
+
+    /*
+    *   onLoad calls
+    */
+    useEffect(() => {
+        handleScroll();
+    }, []);
+
     return (
 
         <div className='skill-modal-container'>
             <div className='text-box-container' style={{width: '100%', height: '100%'}}>
                 <div className= 'text-box-border' />
-                <div className='text-box-content skill-modal-projects'>
+                <div className='text-box-content skill-modal-projects scrollable' ref={scrollListener} onScroll={handleScroll}>
                     {filteredList.map((project, index) =>{
                         return <SkillProjectSummary setModalData={setModalData} project={project} key={`${project.name}${index}`}/>;
                     })}
                 </div>
-                <img className='text-box-nav-up' src='/react-portfolio/Nav-Arrow-1024-1024.png'/>
-                <img className='text-box-nav-down' src='/react-portfolio/Nav-Arrow-1024-1024.png'/>
+                <img className={`text-box-nav-up ${isTop&&'nav-fade'}`} style={{width:'10%'}} src='/react-portfolio/Nav-Arrow-1024-1024.png'/>
+                <img className={`text-box-nav-down ${isBottom&&'nav-fade'}`} style={{width:'10%'}} src='/react-portfolio/Nav-Arrow-1024-1024.png'/>
             </div>
         </div>
 
