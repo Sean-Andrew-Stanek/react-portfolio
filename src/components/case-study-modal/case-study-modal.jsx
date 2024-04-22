f6b976fe27c3e418ef1ce42f7f60ed188996e346
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-04-22 22:02:20 +0900

    pulled requested cases study from utils obj array

--- src/components/case-study-modal/case-study-modal.jsx
+++ src/components/case-study-modal/case-study-modal.jsx
@@ -10,13 +10,16 @@ import React from 'react';
 import './case-study-modal.scss';
 import '../../styles/styles.scss';
 import PropTypes from 'prop-types';
+import { caseStudies } from '../../utils/case-studies';
 
 export const CaseStudyModal = ({modalData}) => {
+
+    const caseStudy = caseStudies.find(obj => obj.name === modalData.data.name);
 
     return (
         <>
             <div className='csm-main-container' style={{backgroundColor:'white'}}>
-                {`This is a case-study about ${modalData.data.name} `}
+                {`This is a case-study about ${caseStudy.name}`}
             </div>
         </>
     );
