655ea229f65247481704eef4978338ab566bf379
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-04-22 11:50:32 +0900

    boilerplate case study

---
+++ src/components/case-study-modal/case-study-modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './case-study-modal.scss';
+
+export const CaseStudyModal = () => {
+
+    return (
+        <>
+            <div style={{backgroundColor:'white'}}>
+                This is a case-study.
+            </div>
+        </>
+    );
+
+};
