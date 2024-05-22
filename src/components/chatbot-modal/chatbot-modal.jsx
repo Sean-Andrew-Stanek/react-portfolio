5603572a95a57e1c288bb2c94db82ba5d40db40d
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-05-22 21:16:56 +0900

    Chatbox look setup

--- src/components/chatbot-modal/chatbot-modal.jsx
+++ src/components/chatbot-modal/chatbot-modal.jsx
@@ -12,14 +12,28 @@ import { images } from '../../utils/images';
 export const ChatBotModal = () => {
 
     return (
-        <div className = 'cbm-main-container cbm-modal-background'>
-            <img className = 'cbm-spear-left' src = {images.spearVertical}/>
-            <img className = 'cbm-spear-right' src = {images.spearVertical}/>
-            <img className = 'cbm-spear-top' src = {images.spearHorizontal}/>
-            <div className = 'cbm-chat-window-container'>
+        <div className='cbm-modal-background'>
+            <div className = 'cbm-main-container'>
+                <img className = 'cbm-spear-left' src = {images.spearVertical}/>
+                <img className = 'cbm-spear-right' src = {images.spearVertical}/>
+                <img className = 'cbm-spear-top' src = {images.spearHorizontal}/>
+                <div className = 'cbm-chat-window-container'>
+                    <div className='cbm-chat-window'>
 
-            </div>
+                    </div>
+                    <div className='cbm-user-bar'>
+                        <div className='cbm-input-box'>
+
+                        </div>
+                        <div className='cbm-submit-button'>
+                            Submit
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>            
         </div>
+
     );
 
 };
