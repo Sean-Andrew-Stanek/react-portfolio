644f0ec6fb403e5d7909675e474912070529e1aa
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-06-17 10:34:33 +0900

    chatbot requires config.js handles it missing

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,9 +1,21 @@
-
 //Local Funct
 //let azureFunctionURI = 'http://localhost:7071/api/get_message';
 
 //Azure Funct
-let azureFunctionURI = 'https://portfolio-chat-bot.azurewebsites.net/api/get_message?code=McshsKnkJmUYmpJGjc7-R4Xg0J-xU1pZEmkI1OYIKoVoAzFutZgr8A%3D%3D';
+
+let azureFunctionURI;
+
+try {
+    const config = await import('./config.js');
+    azureFunctionURI = config.chatbotURI;
+} catch (error) {
+    console.warn(
+        'Failed to load config.js.  Chatbot requires a working function.  See README.md for more details.',
+    );
+    azureFunctionURI = '';
+}
+
+export let isFunction = azureFunctionURI ? azureFunctionURI : false;
 
 /*******************************************/
 /*  messages: old messages                 */
@@ -11,19 +23,18 @@ let azureFunctionURI = 'https://portfolio-chat-bot.azurewebsites.net/api/get_mes
 /*******************************************/
 
 export const getResponseFromOpenAI = async (messages, new_message) => {
-    
     const jsonPayload = {
         messages: messages,
-        new_message: new_message
+        new_message: new_message,
     };
 
     try {
         const response = await fetch(azureFunctionURI, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(jsonPayload)
+            body: JSON.stringify(jsonPayload),
         });
 
         if (!response.ok) {
